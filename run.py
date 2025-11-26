c268b9416cf0fc3983821e053ce6dba957e7e929
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-11-26 20:27:17 +0200

    Implement LazyProperty

--- run.py
+++ run.py
@@ -3,5 +3,41 @@
 # PYTHON_ARGCOMPLETE_OK
 
 
+class LazyProperty:
+    def __init__(self, func):
+        self.func = func
+        self._name = func.__name__
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        instance.__dict__[self._name] = self.func(instance)
+        return instance.__dict__[self._name]
+
+
+def fibonacci(n):
+    if n in (0, 1):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def factorial(n):
+    if n in (0, 1):
+        return 1
+    return n * factorial(n - 1)
+
+
+class SomeMath:
+    @LazyProperty
+    def fib(self):
+        return fibonacci(30)
+
+    @LazyProperty
+    def fac(self):
+        return factorial(50)
+
+
 if __name__ == "__main__":
-    pass
+    somemath = SomeMath()
+    print(somemath.fib)
+    print(somemath.fac)
